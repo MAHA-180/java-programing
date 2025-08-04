41191db1013d0c66a595eb5c28b92822ee154b07
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-04 19:47:54 +0530

    Create statement.java

---
+++ statement.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+       int A=20;
+       int B=19;
+       if (A>B){
+        System.out.println("A is elder");
+       }
+       else
+       {
+        System.out.println("B is elder");
+       }
+    }
+}
