5e4e87b99fa55741c4c6e3cea8920c61916eb9e5
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-05 12:06:10 +0530

    Create reverse count.java

---
+++ reverse count.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        
+        for(int count=10;count>=1;count--){
+            if (count==5){
+                
+            }
+             System.out.println(count);
+            
+        }
+        
+    } 
+}
