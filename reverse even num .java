8e3418cef9a07d75e0dad122759a1dcf79c45d77
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-05 12:11:24 +0530

    Create reverse even num .java

---
+++ reverse even num .java
@@ -0,0 +1,20 @@
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        
+        for(int count=10;count>=1;count--){
+            if (count%2==0){
+             
+            System.out.println(count);   
+            }
+             
+            
+        }
+        
+    } 
+}
