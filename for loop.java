8f039956acc1af2f1e1bf2295ce3f62d2a262c29
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-05 11:43:44 +0530

    Create for loop.java

---
+++ for loop.java
@@ -0,0 +1,21 @@
+syntax:
+for(int count=1;count<=10;count++)
+        {
+            System.out.println(count);
+        }
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner data = new Scanner(System.in);
+        
+        for(int count=1;count<=10;count++)
+        {
+            System.out.println(count);
+        }
+        
+    } 
+}
