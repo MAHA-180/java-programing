19daefd59d2940e4f0e7180c6aa26aeeb7073890
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-05 11:20:25 +0530

    Create do while loop.java

---
+++ do while loop.java
@@ -0,0 +1,33 @@
+
+syntax:
+do
+{
+   //loop block
+}while(boolean_expression);
+
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner data = new Scanner(System.in);
+        System.out.println("enter the number");
+        num = data.nextInt();
+        do
+        {
+            if (num % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+        while (num >= 0) ;
+        
+    } 
+}
+
+
+
