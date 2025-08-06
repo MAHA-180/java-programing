bba6a381d490717d262da3af7af54da3de8e84cd
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-06 09:50:10 +0530

    Update array.java

--- array.java
+++ array.java
@@ -6,7 +6,7 @@
 
 
 
-
+#normal
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
@@ -14,5 +14,43 @@ class Main {
     "asmi","lali","arch"};
         array[1]="jazs";
         System.out.println(array[0]);
+    }
+}
+
+#for 
+class test
+{
+ public static void print(int[] array)
+ {
+ for(int i= 0; i < array.length; i++)
+ System.out.println(array[i]);
+ }
+ public static void main(String [] args)
+ {
+ int[] myarray = {1, 2, 3, 4, 5};
+ print(myarray);
+ }
+}
+
+#if
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        String[] array = {"devi", "pravee", "asmi", "lali", "arch", "maha"};
+        array[1] = "arch"; 
+        System.out.println(array[1]); 
+        
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+            
+            if (array[i].equals("asmi")) {
+                array[i] = "lali";
+            }
+        }
+
+        System.out.println("Updated array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
