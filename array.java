b3e7127e0e4394703daf5ffdec3e76fa2bbe3a36
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-06 09:34:57 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        String[] array={"sanzs","jazs","devi","pravee",
+    "asmi","lali","arch"};
+        array[1]="jazs";
+        System.out.println(array[0]);
+    }
+}
