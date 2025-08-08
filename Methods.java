c27123fc9fd2fcb85c6613ee6d64550e6a45604a
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-08 10:32:04 +0530

    Update and rename Method.java to Methods.java

--- Methods.java
+++ Methods.java
@@ -61,4 +61,37 @@ Enter last name: LAKSHMI
 Full Name: MAHA LAKSHMI
 
 #BIO
+import java.util.Scanner;
+public class Main {
+    static void myMethod(String firstName, String lastName, int age, double height) {
+         System.out.println("\n----BIO----");
+        System.out.println("Full Name: " + firstName + " " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.print("Enter the first name: ");
+        String first = data.next();
+        System.out.print("Enter the last name: ");
+        String last = data.next();
+        System.out.print("Enter the Age: ");
+        int age = data.nextInt();
+        System.out.print("Enter the height: ");
+        double height = data.nextDouble();
+        myMethod(first, last, age, height);
+        
+    }
+}
+OUTPUT:
+Enter the first name: MAHALAKSHMI
+Enter the last name: S
+Enter the Age: 19
+Enter the height: 152.3
+
+----BIO----
+Full Name: MAHALAKSHMI S
+Age: 19
+Height: 152.3
+
 
