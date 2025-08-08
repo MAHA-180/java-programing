8d7ab4b8cedf342a419a5b6d509d347868b093e9
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-08 10:42:46 +0530

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -94,4 +94,52 @@ Full Name: MAHALAKSHMI S
 Age: 19
 Height: 152.3
 
+#CALCULATOR FUNCTION:
+import java.util.Scanner;
+
+public class Calculator {
+    static int add(int a, int b) {
+        return a + b;
+    }
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = data.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = data.nextInt();
+        System.out.print("Enter operation (+, -, *): ");
+        char op = data.next().charAt(0);
+        int result = 0;
+        switch (op) {
+            case '+':
+                result = add(num1, num2);
+                break;
+            case '-':
+                result = subtract(num1, num2);
+                break;
+            case '*':
+                result = multiply(num1, num2);
+                break;
+           
+            default:
+                System.out.println("Invalid operation!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
+
+OUTPUT:
+Enter first number: 5
+Enter second number: 5
+Enter operation (+, -, *): *
+Result: 25
+
 
