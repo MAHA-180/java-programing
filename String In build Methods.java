74cc10dff8c39d3d390a36ed83effd76c0f0a44d
MAHA-180 <mahalakshmi9309@mountzion.ac.in>
2025-08-13 12:14:29 +0530

    Update String In build Methods.java

--- String In build Methods.java
+++ String In build Methods.java
@@ -72,3 +72,162 @@ Enter Name2:
 SATHIAVADIVELAN
 The number is: 4
 Result:-6
+
+#regionMatches
+public class Main {
+    public static void main(String[] args) {
+        String a="Hello Maha";
+        String b="World Asmitha";
+        String x = "Maha|Asmitha|rajesh";
+        System.out.println("Maha".matches(x));
+        System.out.println("Asmitha".matches(x));
+        System.out.println("Java".matches(x));
+        System.out.println(a.regionMatches(7,a,4,5));
+    }
+}
+OUTPUT:
+true
+true
+false
+false
+
+#replace,replaceALL,replaceFirst
+public class Main {
+    public static void main(String[] args) {
+        String a="Hello";
+        String b="Hello Maha Hello";
+        System.out.println(a.replace('L','p'));
+        String x="(?i)Hello";
+        System.out.println(b.replaceAll(x,"Hi"));
+        String y="Hello";
+        System.out.println(b.replaceFirst(y,"Hi"));
+    }
+}
+
+OUTPUT:
+Heppo
+Hi Maha Hi
+Hi Maha Hello
+
+#
+public class Main {
+    public static void main(String[] args) {
+        String x="Hello World";
+        String y="[,\\.\\s]";
+        String[] z=x.split(y);
+        for(String  s:z){
+        System.out.print(s);
+    }
+}
+}
+OUTPUT:
+HelloWorld
+
+#SPLIT,CONCATE
+ import java.util.Scanner;  
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String x = sc.nextLine();
+
+        String y = "[\\.\\s]";  
+        String[] z = x.split(y);
+
+        String concatenated = "";
+
+        System.out.println("Split parts:");
+        for (String s : z) {
+            System.out.println(s);
+            concatenated += s;
+        }
+
+        System.out.println("Concatenated string: " + concatenated);
+    }
+}
+OUTPUT:
+Enter a string: HELLO WORLD
+Split parts:
+HELLO
+WORLD
+Concatenated string: HELLOWORLD
+
+#startswith
+ public class Main {
+    public static void main(String[] args) {
+        String a="Hello";
+        System.out.println(a.startsWith("Hello"));
+        System.out.println(a.startsWith("World"));
+        System.out.println(a.startsWith("He"));
+    }
+}
+OUTPUT:
+true
+false
+true
+
+#
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        String x = "hello world";
+        char[] y = x.toCharArray();   
+        System.out.println(y[4]);    
+    }
+}
+OUTPUT:
+o
+
+#
+class Main {
+    public static void main(String[] args) {
+        String x = "hello world";
+        char[] y = x.toCharArray();   
+
+        for (int i = 0; i < y.length; i++) {
+            System.out.print(y[i]); 
+        }
+    }
+}
+OUTPUT:
+hello world
+
+#Userinput
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter a string: ");
+        String x = scanner.nextLine();          
+        char[] y = x.toCharArray();
+        for (int i = 0; i < y.length; i++) {
+            System.out.print(y[i]);               
+        }
+        
+        scanner.close();  
+    }
+}
+output:
+Enter a string: Hello world
+Hello world
+
+#
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter a string: ");
+        String x = scanner.nextLine();          
+            System.out.println(x); 
+            System.out.println(x.trim()); 
+            System.out.println(String.valueOf(x));    
+                 
+        }
+    }
+OUTPUT:
+Enter a string:        Maha
+       Maha
+Maha
+       Maha
